13bcf505f5f8d0b2a278444b619010f578611e66
Dalton Alexandre <alexandrefamilyfarm@icloud.com>
2024-05-06 11:20:56 -0700

    commit site

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,14 +1,20 @@
 import Leaf
 import Vapor
 
-// configures your application
+ // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.views.use(.leaf)
-
-
-    // register routes
-    try routes(app)
+  // uncomment to serve files from /Public folder
+  // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+ let file = FileMiddleware(publicDirectory: app.directory.publicDirectory)
+ app.middleware.use(file)
+ app.views.use(.leaf)
+ app.routes.defaultMaxBodySize = "11mb"
+// app.http.server.configuration.hostname = "0.0.0.0"
+ app.http.server.configuration.port = 8081
+ print("\(app.directory.workingDirectory)")
+ print("\(app.directory.publicDirectory)")
+ print("\(app.directory.resourcesDirectory)")
+ print(app.routes.all)
+  // register routes
+ try routes(app)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,56 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
-    }
+// app.get { req async in
+//           "It works!"
+//       }
+//    app.get { req async throws in
+//        try await req.view.render("index", ["title": "Hello Vapor!"])
+//    }
+// app.get("hello") { req async throws -> View in
+//     return try await req.view.render("hello", ["name": "Leaf"])
+//    }
+//
+ app.get("master") { req -> EventLoopFuture<View> in
+  return req.view.render("master")
+ }
+ 
+ app.get("index") { req -> EventLoopFuture<View> in
+  return req.view.render("index")
+ }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
+ app.get { req -> EventLoopFuture<View> in
+  return req.view.render("yourdisciple")
+ }
+ 
+// app.get("stream-file") { req -> EventLoopFuture<Response> in
+//  let filePath = app.directory.publicDirectory + "/YourDisciple.m4a"
+//  return req.fileio.streamFile(at: filePath) { result in
+//   switch result {
+//    case .success: break
+//      // Handle success (e.g., log completion)
+//    case .failure(let error): break
+//      // Handle failure (e.g., log error)
+//   }
+//  }
+// }
+ 
+ app.get("stephenethridge", "yourdisciple") { req -> EventLoopFuture<View> in
+  return req.view.render("audio")
+ }
+ 
+// app.get("play-audio") { req -> Response in
+//  let audioFilePath = app.directory.publicDirectory + "/Left Hand Cramps.mp3" // Replace with the actual file path
+//  let res = Response()
+//  res.headers.add(name: .contentType, value: "audio/mpeg")
+//  res.headers.add(name: .contentDisposition, value: "inline")
+//  res.body = .init(stringLiteral: audioFilePath)
+//  return res
+// }
+ 
+ app.get("play") { req -> Response in
+  let audioFilePath = app.directory.publicDirectory + "/Left Hand Cramps.m4a"
+//  let songID = req.parameters.get("songID")!
+  return req.fileio.streamFile(at: audioFilePath)
+ }
 }
